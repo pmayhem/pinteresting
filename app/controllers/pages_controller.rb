e8ffc59fd8f62aa6d21ce7f314a61394adcce791
Menandro Malansacay <mmadlansacay@gmail.com>
2014-04-08 23:24:19 -0700

    .

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def home
+  	#everything between this has to do with the homepage
+  end
+   
+   def about
+  	#everything between this has to do with the homepage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home" #changed to root
+  get "about"=>"pages#about"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
