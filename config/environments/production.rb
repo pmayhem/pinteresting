0f1ea41ce3bd8a039b34c56710ac313f315aa353
Menandro Malansacay <mmadlansacay@gmail.com>
2014-04-20 02:31:39 -0700

    add amazon 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinteresting::Application.configure do
 
 #Note to set this to actual hostname
   config.action_mailer.default_url_options = { :host => 'mayhem-pinteresting.herokuapp.com' }
+
+    # set Paperclip to upload images to Amazon S3
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
 end
